20633c464d4f6619e7cec90b1fd968957efbb1e5
Yonny Rodriguez <jhrodrigu@hotmail.com>
2020-04-24 01:07:43 -0500

    calculadora

---
+++ main.js
@@ -0,0 +1,7 @@
+function init() {
+
+    var resultado = document.getElementById("resultado");
+    var dividir = document.getElementById("dividir");
+    var multi = document.getElementById("multi");
+
+}
